2e447fb700444a0f7ea397006916d6303ba2d218
Marc Schär <marcschaer@Marcs-MacBook-Pro.local>
2024-01-04 08:47:17 +0100

    psql render settings

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -2,8 +2,8 @@ import axios from "axios";
 import { jwtDecode } from "jwt-decode";
 
 // const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:3001";
-const BASE_URL = "http://localhost:3001";
-// const BASE_URL = "https://react-jobly.onrender,com";
+// const BASE_URL = "http://localhost:3001";
+const BASE_URL = "https://react-jobly-backend-e7vv.onrender.com/";
 
 /** API Class.
  *
