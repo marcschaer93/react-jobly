89c4d9d11ed0f77210463c675b732c458a28446e
Marc Schär <marcschaer@Marcs-MacBook-Pro.local>
2024-01-04 10:44:24 +0100

    vite base config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: `https://react-jobly-backend-e7vv.onrender.com`,
 });
